338d4f1d46695df826628820a46addbd175f99b2
Mike McBride <mike.mcbride@wwt.com>
2016-08-01 21:02:30 -0500

    uppercaserer

---
+++ 12-http-uppercaserer.js
@@ -0,0 +1,13 @@
+'use strict'
+const http = require('http')
+const map = require('through2-map')
+
+const server = http.createServer((req, res) => {
+  if (req.method !== 'POST') {
+    return res.end('I only accept POST\n')
+  }
+  req.pipe(map(chunk => chunk.toString().toUpperCase()))
+    .pipe(res)
+})
+
+server.listen(Number(process.argv[2]))
